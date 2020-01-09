1342399cd18b69d61aeb65533036b639a8129a3e
Danar Widi <widiu7omo@gmail.com>
2020-01-09 19:02:25 +0800

    add log and compare between img and pdf

--- index.js
+++ index.js
@@ -2,24 +2,36 @@ const {PDFDocument} = require('pdf-lib');
 const fs = require('fs');
 const baseUrl = process.cwd();
 const imgExtension = ".jpg";
-const imgFolderName = 'vklaim-img';
-const pdfFolderName = 'scanned-pdf';
+const imgFolderName = '/vklaim-img';
+const pdfFolderName = '/scanned-pdf';
 
 function getExceptionPdf() {
     const data = fs.readFileSync(`${baseUrl}/exception.txt`);
     return data.toString().split(',');
 }
 
-function initScannedFiles() {
-    return new Promise(function (resolve,reject) {
-        fs.readdir(baseUrl+pdfFolderName,function (err,data) {
-            if(err){
-                reject(err)
-            }
-            else{
-                data.map(function(item){
+function sortAsc(a, b) {
+    return a - b;
+}
 
+//check who's left scanned pdf
+Array.prototype.diff = function (arr2) {
+    return this.filter(x => !arr2.includes(x));
+}
+
+function initScannedFiles() {
+    return new Promise(function (resolve, reject) {
+        fs.readdir(baseUrl + pdfFolderName, function (err, data) {
+            if (err) {
+                reject(err);
+            } else {
+                const noPdf = data.map(function (item) {
+                    const arrayItem = item.toString().split('.');
+                    if (arrayItem.length > 0) {
+                        return parseInt(arrayItem[0]);
+                    }
                 })
+                resolve(noPdf);
             }
         })
     })
@@ -28,12 +40,11 @@ function initScannedFiles() {
 function initImgFiles() {
     return new Promise(function (resolve, reject) {
         fs.readdir(`${baseUrl}/vklaim-img`, function (err, data) {
-            if(err){
+            if (err) {
                 reject(err)
-            }
-            else{
+            } else {
                 const dataImg = data.map(function (item) {
-                    return item.replace(imgExtension, "");
+                    return parseInt(item.replace(imgExtension, ""));
                 });
                 resolve(dataImg)
             }
@@ -42,10 +53,22 @@ function initImgFiles() {
 
 }
 
+
 async function createPDF() {
     const exception = await getExceptionPdf();
     const initImg = await initImgFiles();
-    console.log(initImg);
+    const initPdf = await initScannedFiles();
+    console.log(initImg.sort(sortAsc));
+    console.log(initPdf.sort(sortAsc));
+    //get differences
+    const differences = initImg.sort(sortAsc).diff(initPdf.sort(sortAsc));
+    console.log(differences);
+    console.log(initImg.length - initPdf.length);
+    //write log pdf
+    fs.writeFileSync(baseUrl + '/pdfExist.log', initPdf.sort(function (a, b) {
+        return a - b;
+    }).join("\n"));
+    fs.writeFileSync(baseUrl + '/imgExist.log', initImg.join("\n"));
     const filePDF = await PDFDocument.create();
     const page = filePDF.addPage();
     page.drawText("Testing Development");
